3571641f6be62befdbcb2db0750f835fd9f7d214
Jose Luis Mendoza Albaladejo <joseluismendoza97@gmail.com>
2021-09-16 18:36:21 +0200

    hecho todo de manera simple

---
+++ src/app/layout/cabecera/cabecera.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecera',
+  templateUrl: './cabecera.component.html',
+  styleUrls: ['./cabecera.component.scss'],
+})
+export class CabeceraComponent implements OnInit {
+  public empresa = 'indra';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/titulo/titulo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-titulo',
+  templateUrl: './titulo.component.html',
+  styleUrls: ['./titulo.component.scss']
+})
+export class TituloComponent{
+
+  title = 'App de JL'
+
+}
